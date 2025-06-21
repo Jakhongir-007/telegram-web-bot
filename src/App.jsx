cb8f92d102259ccc218af76a7a6c7553ff309030
Jakhongirbey <q.jakhongirbey@gmail.com>
2025-06-21 12:14:30 +0500

    push

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/card/card";
+import Cart from "./components/cart/cart";
+import { getData } from "./constants/db";
+
+const courses = getData();
+
+const App = () => {
+    const [cartItems, setCartItems] = useState([]);
+
+    const telegram = window.Telegram.WebApp;
+
+    useEffect(() => {
+        telegram.ready();
+    });
+
+    const onAddItem = (item) => {
+        const existItem = cartItems.find((c) => c.id == item.id);
+        console.log("EXIST_ITEM", existItem);
+
+        if (existItem) {
+            const newData = cartItems.map((c) =>
+                c.id == item.id
+                    ? { ...existItem, quantity: existItem.quantity + 1 }
+                    : c
+            );
+            console.log("ADD_QUANTITY_EXIST_ITEM", newData);
+            setCartItems(newData);
+        } else {
+            const newData = [...cartItems, { ...item, quantity: 1 }];
+            console.log("ADD_ITEM", newData);
+            setCartItems(newData);
+        }
+    };
+    const onRemoveItem = (item) => {
+        const existItem = cartItems.find((c) => c.id == item.id);
+        console.log("existItem", existItem);
+
+        if (existItem.quantity === 1) {
+            const newData = cartItems.filter((c) => c.id !== item.id);
+            console.log("DELETE_ITEM_QUANTITY_0", newData);
+            setCartItems(newData);
+        } else {
+            const newData = cartItems.map((c) =>
+                c.id === existItem.id
+                    ? { ...existItem, quantity: existItem.quantity - 1 }
+                    : c
+            );
+            console.log("DELETE_ONE_ITEM");
+            setCartItems(newData);
+        }
+    };
+
+    const onCheckout = () => {
+        telegram.MainButton.text = "Sotib olish :)";
+        telegram.MainButton.show();
+    };
+
+    return (
+        <>
+            <h1 className="heading">English courses</h1>
+            <Cart cartItems={cartItems} onCheckout={onCheckout} />
+            <div className="cards_container">
+                {courses.map((course) => (
+                    <Card
+                        key={course.id}
+                        course={course}
+                        onAddItem={onAddItem}
+                        onRemoveItem={onRemoveItem}
+                    />
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default App;
---
+++ src/constants/db.js
@@ -0,0 +1,22 @@
+export function getData() {
+    return [
+        {
+            title: "Beginner English",
+            price: 10,
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzdr71-yDVnl9Os3BdjwVHTUiO7slEf-SAOg&s",
+            id: 1,
+        },
+        {
+            title: "Intermediate English",
+            price: 15,
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7SEY-4xYuSIDtnLND0rTbh_SESdZfeYuAmw&s",
+            id: 2,
+        },
+        {
+            title: "Advanced Speaking",
+            price: 20,
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaVVY1wRS_XT4OVx4dMXmDHJqOHNaPyi5Yfw&s",
+            id: 3,
+        },
+    ];
+}
